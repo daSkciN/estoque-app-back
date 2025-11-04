17ff8d2aaeb5ee164bdaee07a3cc226f045d73fa
daSkciN <nicolas.s.araujo2003@gmail.com>
2025-11-03 23:03:55 -0300

    adicionado get de produto com estoque baixo

--- src/main/java/com/estoqueApp/estoqueApp/repository/ProdutoRepository.java
+++ src/main/java/com/estoqueApp/estoqueApp/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.estoqueApp.estoqueApp.repository;
 import com.estoqueApp.estoqueApp.entity.Produto;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +13,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     boolean existsByNomeIgnoreCase(@NotBlank(message = "O nome do produto é obrigatório.") String nome);
 
     boolean existsByNomeIgnoreCaseAndIdProdutoNot(@NotBlank(message = "O nome do produto é obrigatório.") String nome, Integer idProduto);
+
+    @Query("SELECT COUNT(p) FROM Produto p WHERE p.quantidadeEstoque < :limite")
+    Long countProdutosComEstoqueBaixo(@Param("limite") Integer limite);
 }
